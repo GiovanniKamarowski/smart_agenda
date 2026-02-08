2597400a3e15d7490f85c598a1b4d92aaa5342ef
JALLCARD\giovanni.dias <giovanni.kds@gmail.com>
2026-02-08 13:01:58 -0300

    Fix React Query provider setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import './index.css'
+
+// Cria uma instância do QueryClient
+const queryClient = new QueryClient()
 
 // Renderiza o componente App na div com id 'root' do HTML
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
     </React.StrictMode>,
 )
